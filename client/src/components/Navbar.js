c9404a15ebf76c016e91aeed15398b8d25a5efe2
Raul Valdez <rvaldez20@gmail.com>
2021-12-22 14:33:38 -0600

    Se agrega el NavBar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,10 +1,34 @@
-import {Button} from '@mui/material'
+import { AppBar, Box, Button, Container, Toolbar, Typography } from '@mui/material';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function Navbar() {
+
+   const navigate = useNavigate();
+
    return (
-      <div>
-         Navbar
-         <Button variant='contained' color='secondary'>Click Me</Button>
-      </div>
+      <Box sx={{flexGrow: 1}}>
+         <AppBar position='static' color='transparent'>
+            <Container>
+               <Toolbar>
+                  <Typography 
+                     variant='h6'
+                     sx={{flexGrow: 1}}                     
+                  >
+                     <Link to='/' style={{ textDecoration: "none", color: "#eee" }}>
+                        PERN Stack
+                     </Link>
+                  </Typography>
+
+                  <Button 
+                     variant='contained'
+                     color='primary'
+                     onClick={() => navigate('/tasks/new')}
+                  >
+                     New Task
+                  </Button>
+               </Toolbar>
+            </Container>
+         </AppBar>
+      </Box>
    )
 }
