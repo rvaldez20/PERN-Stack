5767414c74af397f467852927bc9d7124a6fb613
Raul Valdez <rvaldez20@gmail.com>
2021-12-22 13:53:10 -0600

    Se configura muiMaterial container

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,8 +1,10 @@
+import {Button} from '@mui/material'
 
 export default function Navbar() {
    return (
       <div>
          Navbar
+         <Button variant='contained' color='secondary'>Click Me</Button>
       </div>
    )
 }
